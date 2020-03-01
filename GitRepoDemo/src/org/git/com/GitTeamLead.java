44a0a101d72aa396b3a6b1e58e8de643b1d5b6b6
Nitin <Nitin@Nihaar>
2020-03-01 16:32:09 +0530

    first commit

---
+++ GitRepoDemo/src/org/git/com/GitTeamLead.java
@@ -0,0 +1,10 @@
+package org.git.com;
+
+public class GitTeamLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Team Lead push the project");
+}
+
+}
